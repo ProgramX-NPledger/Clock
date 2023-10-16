986cd80f1e740f4478f4456705997886de0a9f4f
Nathan Pledger <nathan@programx.co.uk>
2023-10-16 22:02:17 +0100

    Initial commit

---
+++ src/Clock/Clock.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Clock.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
