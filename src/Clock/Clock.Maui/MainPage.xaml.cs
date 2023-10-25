0174da4a7cf084ebce09e729b7eb4a7f1986edb5
Nathan Pledger <nathan@programx.co.uk>
2023-10-25 16:46:56 +0100

    Implementation of MVVM

--- src/Clock/Clock.Maui/MainPage.xaml.cs
+++ src/Clock/Clock.Maui/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Clock.Maui.Data;
 using Clock.Maui.Model;
+using Clock.Maui.ViewModel;
 using SQLite;
 
 namespace Clock.Maui;
@@ -7,100 +8,47 @@ namespace Clock.Maui;
 public partial class MainPage : ContentPage
 {
     private IDispatcherTimer _mainClock;
-    private DateTime _mainClockLastStartedAt;
+
     
     public MainPage()
     {
-        
         InitializeComponent();
+        //BindingContext = new MainViewModel(); // could use this to use a non-default ctor
+        
+        _mainClock = CreateMainClock();
+        ((MainViewModel)BindingContext).RequestMainTimerStart += (s, e) =>
+        {
+            _mainClock.Start();
+        };
+        ((MainViewModel)BindingContext).RequestMainTimerStop += (s, e) =>
+        {
+            _mainClock.Stop();
+        };
+
     }
 
    
+    private IDispatcherTimer CreateMainClock()
+    {
+        if (Application.Current != null)
+        {
+            IDispatcherTimer dispatcherTimer =Application.Current.Dispatcher.CreateTimer();
+			
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+            dispatcherTimer.Tick += (sender, args) =>
+            {
+                var viewModel = this.BindingContext;
+                ((MainViewModel)viewModel).MainTimerValue = DateTime.Now - ((MainViewModel)viewModel).MainTimerLastStartedAt;
+//				 ToString("h\\:mm\\:ss");
+            };
+
+            return dispatcherTimer;
+
+        }
+        throw new InvalidOperationException("Failed to create Main Clock because Application.Current is null");
+    }
+    
 
  
 
-    private void StartStopButton_OnClicked(object sender, EventArgs e)
-    {
-        StopOrStartMainClock();
-    }
-
-    private void StopOrStartMainClock()
-    {
-        if (_mainClock is { IsRunning: true })
-        {
-            StopMainClock();
-            AddCurrentWorkItemToDatabase();
-        }
-        else
-        {
-            StartMainClock();
-        }
-    }
-
-    private void AddCurrentWorkItemToDatabase()
-    {
-        App.WorkItemRepository.AddCurrentWorkItemToDatabase(new WorkItem()
-        {
-            Title = WorkItemEntry.Text,
-            RecordedTime = CalculateMainTimerDifference(),
-            StartTime = _mainClockLastStartedAt,
-            StopTime = DateTime.Now
-        });
-    }
-
-    private void WorkItemEntry_OnCompleted(object sender, EventArgs e)
-    {
-        // user has pressed Enter on textbox, so start the clock
-        Entry entry = (Entry)sender;
-
-        // if the clock isnot running, start it
-        if (_mainClock is { IsRunning: false })
-        {
-            StartMainClock();
-        }
-        else
-        {
-            // if it's already running, we should add the current work item, stop the clock and restart it with the new WorkItem
-            
-        }
-
-    }
-
-    private void StartMainClock()
-    {
-        if (_mainClock == null)
-        {
-            if (Application.Current != null) _mainClock = Application.Current.Dispatcher.CreateTimer();
-        }
-
-        if (_mainClock == null) throw new NullReferenceException("Failed to define _mainClock");
-        
-        _mainClock.Interval = TimeSpan.FromSeconds(1);
-        _mainClock.Tick += MainClock_Tick;
-
-        StartStopButton.Text = "Stop";
-        _mainClock.Start();
-        _mainClockLastStartedAt = DateTime.Now;
-        //this.Resources["Timer"] = "00:00:01"; // setting a dynamic resource
-    }
-
-    private void StopMainClock()
-    {
-        if (_mainClock == null) return;
-        
-        _mainClock.Stop();
-        _mainClock.Tick -= MainClock_Tick;
-        WorkItemEntry.Text = "";
-        StartStopButton.Text = "Start";
-    }
-
-    private void MainClock_Tick(object sender, EventArgs e)
-    {
-        ClockLabel.Text = CalculateMainTimerDifference().ToString("h\\:mm\\:ss");
-    }
-
-    private TimeSpan CalculateMainTimerDifference()
-    {
-        return DateTime.Now - _mainClockLastStartedAt;
-    }
 }
---
+++ src/Clock/Clock.Maui/ViewModel/MainViewModel.cs
@@ -0,0 +1,189 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+//using Android.Locations;
+using Clock.Maui.Model;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Clock.Maui.ViewModel;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+	private DateTime _mainTimerLastStartedAt;
+	private TimeSpan _mainTimerValue;
+	private string _workItemEntry;
+	private string _mainTimerButtonText;
+	private bool _isMainTimerRunning;
+
+	public event EventHandler<EventArgs> RequestMainTimerStart;
+	public event EventHandler<EventArgs> RequestMainTimerStop;
+	
+	public bool IsMainTimerRunning
+	{
+		get => _isMainTimerRunning;
+		set
+		{
+			if (_isMainTimerRunning != value)
+			{
+				_isMainTimerRunning = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+	
+
+	public DateTime MainTimerLastStartedAt
+	{
+		get => _mainTimerLastStartedAt;
+		set
+		{
+			if (_mainTimerLastStartedAt != value)
+			{
+				_mainTimerLastStartedAt = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+	
+	public string MainTimerButtonText
+	{
+		get => _mainTimerButtonText;
+		set
+		{
+			if (_mainTimerButtonText != value)
+			{
+				_mainTimerButtonText = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+	
+	public TimeSpan MainTimerValue
+	{
+		get => _mainTimerValue;
+		set
+		{
+			if (_mainTimerValue != value)
+			{
+				_mainTimerValue = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	public string WorkItemEntry
+	{
+		get => _workItemEntry;
+		set
+		{
+			if (_workItemEntry != value)
+			{
+				_workItemEntry = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+	
+	public ICommand StartStopButtonCommand { get; private set; }
+
+	
+	
+	
+	// MVVM requires a default constructor
+	public MainViewModel()
+	{
+		StartStopButtonCommand = new Command(() =>
+		{
+			StopOrStartMainClock();
+		});
+	}
+
+
+
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+
+	protected virtual void OnRequestMainTimerStart(EventArgs e)
+	{
+		if (RequestMainTimerStart != null) RequestMainTimerStart(this,e);
+	}
+
+	protected virtual void OnRequestMainTimerStop(EventArgs e)
+	{
+		if (RequestMainTimerStop != null) RequestMainTimerStop(this,e);
+	}
+	
+	// protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+	// {
+	// 	if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+	// 	field = value;
+	// 	OnPropertyChanged(propertyName);
+	// 	return true;
+	// }
+	
+  
+
+    private void StopOrStartMainClock()
+    {
+        if (IsMainTimerRunning)
+        {
+            StopMainClock();
+            AddCurrentWorkItemToDatabase();
+        }
+        else
+        {
+            StartMainClock();
+        }
+    }
+
+    private void AddCurrentWorkItemToDatabase()
+    {
+        App.WorkItemRepository.AddCurrentWorkItemToDatabase(new WorkItem()
+        {
+            Title = WorkItemEntry,
+            RecordedTime = MainTimerValue,
+            StartTime = _mainTimerLastStartedAt,
+            StopTime = DateTime.Now
+        });
+    }
+
+    private void WorkItemEntry_OnCompleted(object sender, EventArgs e)
+    {
+        // user has pressed Enter on text box, so start the clock
+        Entry entry = (Entry)sender;
+
+        // if the clock is not running, start it
+        if (IsMainTimerRunning)
+        {
+            StartMainClock();
+        }
+        else
+        {
+            // if it's already running, we should add the current work item, stop the clock and restart it with the new WorkItem
+            
+        }
+
+    }
+
+    private void StartMainClock()
+    {
+        MainTimerButtonText = "Stop";
+        
+        MainTimerLastStartedAt = DateTime.Now;
+        OnRequestMainTimerStart(EventArgs.Empty);
+    }
+
+    private void StopMainClock()
+    {
+		OnRequestMainTimerStop(EventArgs.Empty);
+        WorkItemEntry = string.Empty;
+        MainTimerButtonText = "Start";
+    }
+
+  
+	
+}
