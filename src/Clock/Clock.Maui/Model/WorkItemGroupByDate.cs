7e729a7029347e8d29db59b39e68ab92d8a53704
Nathan Pledger <nathan@programx.co.uk>
2023-10-26 13:12:53 +0100

    Implemented grouping

---
+++ src/Clock/Clock.Maui/Model/WorkItemGroupByDate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Clock.Maui.Model;
+
+public class WorkItemGroupByDate : ObservableCollection<WorkItem>
+{
+	public string Name { get; set; }
+	public DateTime Date { get; set; }
+
+	public WorkItemGroupByDate(string name, DateTime date, ObservableCollection<WorkItem> workItems)
+		: base(workItems)
+	{
+		Name = name;
+		Date = date;
+	}
+}
