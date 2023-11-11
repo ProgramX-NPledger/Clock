102387f17b2921ea2cc4fd5fa2e17ed76f8dcd34
Nathan Pledger <nathan@programx.co.uk>
2023-11-11 10:47:28 +0000

    Added model types ready for API implementation

---
+++ src/Clock/Clock.Maui/Model/GitHub/Asset.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+
+namespace Clock.Maui.Model.GitHub;
+
+public class Asset
+{
+    [JsonProperty("url")]
+    public string Url { get; set; }
+    
+    [JsonProperty("id")]
+    public long Id { get; set; }
+    
+    [JsonProperty("node_id")]
+    public string NodeId { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+    
+    [JsonProperty("label")]
+    public string Label { get; set; }
+    
+    [JsonProperty("uploader")]
+    public Author Uploader { get; set; }
+    
+    [JsonProperty("content_type")]
+    public string ContentType { get; set; }
+    
+    [JsonProperty("state")]
+    public string State { get; set; }
+    
+    [JsonProperty("size")]
+    public long Size { get; set; }
+    
+    [JsonProperty("download_count")]
+    public long DownloadCount { get; set; }
+    
+    [JsonProperty("created_at")]
+    public DateTime CreatedAt { get; set; }
+    
+    [JsonProperty("updated_at")]
+    public DateTime UpdatedAt { get; set; }
+    
+    [JsonProperty("browser_download_url")]
+    public string BrowserDownloadUrl { get; set; }
+    
+    
+}
